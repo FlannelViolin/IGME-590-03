1b35042b1097af6f8208d929de8b39ba58783c4a
Student <georgiapartyka@gmail.com>
2014-03-04 09:13:35 -0500

    Added beginning of bride

---
+++ GAPBlackJack3/GameModel.h
@@ -0,0 +1,37 @@
+//
+//  GameModel.h
+//  BlackJack
+//
+//  Created by Student on 2/24/14.
+//  Copyright (c) 2014 Student. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "PlayingCard.h"
+
+static NSString *kNotificationGameDidEnd = @"kNotificationGameDidEnd";
+static const int kMAX_PLAYER_CARDS = 5;
+
+typedef enum : int{
+    kGameStagePlayer,
+    kGameStageDealer,
+    kGameStageGameOver
+}kGameStage;
+
+@interface GameModel : NSObject
+
+@property (nonatomic) kGameStage gameStage;
+@property (nonatomic) int bet;
+@property (nonatomic) int money;
+
+-(void) dealNewHand;
+-(void) updateGameStage;
+-(PlayingCard*) drawDealerCard;
+-(PlayingCard*) drawPlayerCard;
+-(PlayingCard*) lastDealerCard;
+-(PlayingCard*) lastPlayerCard;
+-(PlayingCard*) playerCardAtIndex:(int) index;
+-(PlayingCard*) dealerCardAtIndex:(int) index;
+
+
+@end
